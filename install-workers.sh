347b242d7df95afa4895b99496d233588a2e0435
Kyle McDonald <kyle@kylemcdonald.net>
2024-10-21 15:03:09 +0200

    more automation

---
+++ install-workers.sh
@@ -0,0 +1,24 @@
+SERVICE_ID=transformirror-workers
+SERVICE_NAME="transformirror workers"
+
+USER=$(whoami)
+SERVICES_DIR=/etc/systemd/system/
+
+cat >$SERVICES_DIR/$SERVICE_ID.service <<EOL
+[Unit]
+Description=$SERVICE_NAME
+Wants=network-online.target
+After=network-online.target
+[Service]
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/run-workers.sh
+User=$USER
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOL
+
+systemctl daemon-reload
+
+systemctl enable $SERVICE_ID
+systemctl start $SERVICE_ID
--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ IP_ADDRESS=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=US/ST=State/L=City/O=Organization/OU=OrganizationalUnit/CN=$IP_ADDRESS"
 python3 download_files.py
 
-bash run-workers.sh &
-python3 server.py &
+bash install-workers.sh
+bash install-server.sh
