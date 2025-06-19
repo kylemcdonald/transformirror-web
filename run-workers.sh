4d57231a357abd2a112bf8a6cde724ac597fcef7
Kyle McDonald <kyle@kylemcdonald.net>
2025-06-19 00:14:21 -0700

    generalize run-workers

--- run-workers.sh
+++ run-workers.sh
@@ -10,11 +10,10 @@ trap cleanup SIGINT
 (
     TOTAL_DEVICES=$(python3 -c "import torch; print(torch.cuda.device_count())")
     FINAL_DEVICE=$((TOTAL_DEVICES - 1))
+    USERNAME=$(whoami)
     for i in $(seq 0 $FINAL_DEVICE); do
         echo "Starting worker $i"
-        # export HF_HOME=/workspace/.cache
-        # source venv/bin/activate
-        CUDA_VISIBLE_DEVICES=$i /home/bizon/anaconda3/bin/python3 worker.py &
+        CUDA_VISIBLE_DEVICES=$i /home/$USERNAME/anaconda3/bin/python worker.py &
     done
     wait
 ) &
