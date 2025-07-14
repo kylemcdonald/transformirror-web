208e4578ae951472c612ebaa2e7983fc4010ec69
Kyle McDonald <kyle@kylemcdonald.net>
2025-07-14 16:10:34 -0300

    vnc script

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-x11vnc -display :0 -forever -nopw -shared & 
+
+# Detect current display
+CURRENT_DISPLAY=$(who | cut -d' ' -f2 | grep -E "^:[0-9]+$" | head -1)
+if [ -z "$CURRENT_DISPLAY" ]; then
+    CURRENT_DISPLAY=":0"
+fi
+
+echo "Using display: $CURRENT_DISPLAY"
+x11vnc -display $CURRENT_DISPLAY -forever -nopw -shared & 
