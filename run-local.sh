6162baa8da4aa5f1337316642192976f023ea520
Kyle McDonald <kyle@kylemcdonald.net>
2025-07-14 16:09:55 -0300

    adapt to different display numbers

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Detect current display
+CURRENT_DISPLAY=$(who | cut -d' ' -f2 | grep -E "^:[0-9]+$" | head -1)
+if [ -z "$CURRENT_DISPLAY" ]; then
+    CURRENT_DISPLAY=":0"
+fi
+
+echo "Using display: $CURRENT_DISPLAY"
+
 # source venv/bin/activate
-DISPLAY=:0 \
+DISPLAY=$CURRENT_DISPLAY \
     __GL_SHOW_OVERLAY=0 \
     __GL_SHOW_FPS=0 \
     __GL_SHOW_GRAPHICS_OSD=0 \
